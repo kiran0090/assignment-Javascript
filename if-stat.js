68130acb14c81481c66cf737b40179e8b7d03055
kiran_iyer <54279242+kiran0090@users.noreply.github.com>
2021-01-27 22:36:15 +0530

    Delete if-stat.js

--- if-stat.js
+++
@@ -1,9 +0,0 @@
-let num = parseInt(prompt("Enter the num"));
-
-if (num % 3 == 0) {
-  alert("tic");
-} else if (num % 5 == 0) {
-  alert("tac");
-} else if ((num % 3 !== 0) & (num % 5 !== 0)) {
-  alert("tic tac toe");
-}
